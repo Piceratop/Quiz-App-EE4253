5460025811943fd54bee678e984ff5bbd31a8f8d
Piceratop Finfina <buinhatminh2k52005@gmail.com>
2024-12-16 10:44:30 +0700

    Finish the basic functionality of the question box

--- frontend/src/components/Explore.jsx
+++ frontend/src/components/Explore.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../configs/apiClient";
+import QuestionBox from "./questions/QuestionBox";
 
 /**
  * Fetches questions from the API and returns the response data.
@@ -10,58 +11,6 @@ import apiClient from "../configs/apiClient";
 async function getQuestions(page) {
    const response = await apiClient.get("/questions?page=" + page);
    return response.data;
-}
-
-/**
- * QuestionBox is a component that renders a single question and its choices.
- *
- * It takes 3 props: question, type, and choices. question is the question string,
- * type is the type of the question (currently only supports "MCQ"),
- * and choices is a stringified array of choices.
- *
- * It renders the question in a large, centered box with a border, and then renders
- * the choices in a grid of 4 columns, each choice being a centered, bordered box.
- *
- * @param {{ question: string, type: string, choices: string }} props
- * @returns {JSX.Element}
- */
-function QuestionBox({ question, type, choices }) {
-   const choiceArray = JSON.parse(choices);
-   const [userChoice, setUserChoice] = useState(null);
-
-   if (type === "MCQ") {
-      return (
-         <form className="my-6">
-            <div className="text-xl flex items-center justify-center py-12 px-4 my-2 border-2 border-primary">
-               {question}
-            </div>
-            <div
-               className="grid gap-2"
-               style={{
-                  gridTemplateColumns: `repeat(${choiceArray.length}, 1fr)`,
-               }}
-            >
-               {choiceArray.map((choice) => (
-                  <label
-                     className={`flex items-center justify-center py-8 px-4 border border-primary text-center transition duration-500 ease-in-out ${
-                        userChoice === choice ? "bg-secondary font-bold" : ""
-                     }`}
-                     key={choice}
-                  >
-                     <input
-                        className="appearance-none"
-                        type="radio"
-                        name={`${question}`}
-                        value={choice}
-                        onChange={(e) => setUserChoice(e.target.value)}
-                     />
-                     {choice}
-                  </label>
-               ))}
-            </div>
-         </form>
-      );
-   }
 }
 
 function Explore() {
@@ -83,9 +32,10 @@ function Explore() {
          {Object.entries(questions).map(([id, question]) => (
             <QuestionBox
                key={id}
+               choices={question.choices}
+               correctAnswer={question.correct_answer}
                question={question.question}
                type={question.question_type}
-               choices={question.choices}
             />
          ))}
       </div>
---
+++ frontend/src/components/questions/QuestionBox.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+/**
+ * QuestionBox is a component that renders a single question and its choices.
+ *
+ * It takes 3 props: question, type, and choices. question is the question string,
+ * type is the type of the question (currently only supports "MCQ"),
+ * and choices is a stringified array of choices.
+ *
+ * It renders the question in a large, centered box with a border, and then renders
+ * the choices in a grid of 4 columns, each choice being a centered, bordered box.
+ *
+ * @param {{ question: string, type: string, choices: string }} props
+ * @returns {JSX.Element}
+ */
+export default function QuestionBox({
+   correctAnswer,
+   choices,
+   question,
+   type,
+}) {
+   const choiceArray = JSON.parse(choices);
+   const [isSubmitted, setIsSubmitted] = useState(false);
+   const [userChoice, setUserChoice] = useState(null);
+
+   if (type === "MCQ") {
+      return (
+         <form className="my-6">
+            <div className="my-2 border-primary border-2 transition duration-500 ease-in-out">
+               <div className="text-xl flex items-center justify-center py-12 px-4">
+                  <p>{question}</p>
+               </div>
+               <button
+                  className={`w-full py-2 transition duration-500 ease-in-out ${
+                     isSubmitted
+                        ? "bg-background text-primary "
+                        : userChoice
+                        ? "bg-primary text-background"
+                        : "bg-gray-400 text-background disabled"
+                  }`}
+                  type="submit"
+                  onClick={(e) => {
+                     e.preventDefault();
+                     console.log(userChoice);
+                     if (userChoice) {
+                        setIsSubmitted(true);
+                     }
+                  }}
+                  disabled={isSubmitted || !userChoice}
+               >
+                  {isSubmitted ? "Submitted" : "Submit"}
+               </button>
+            </div>
+            <div
+               className="grid gap-2"
+               style={{
+                  gridTemplateColumns: `repeat(${choiceArray.length}, 1fr)`,
+               }}
+            >
+               {choiceArray.map((choice) => (
+                  <label
+                     className={`flex items-center justify-center py-8 px-4 border border-primary text-center transition duration-500 ease-in-out ${
+                        isSubmitted
+                           ? choice === correctAnswer
+                              ? "bg-primary text-background"
+                              : userChoice === choice
+                              ? "bg-wrong text-background border-wrong"
+                              : ""
+                           : userChoice === choice
+                           ? "bg-secondary"
+                           : ""
+                     } ${userChoice === choice ? "font-bold" : ""}`}
+                     key={choice}
+                  >
+                     <input
+                        className="appearance-none"
+                        type="radio"
+                        name={`${question}`}
+                        value={choice}
+                        onChange={(e) => setUserChoice(e.target.value)}
+                     />
+                     {choice}
+                  </label>
+               ))}
+            </div>
+         </form>
+      );
+   }
+}
